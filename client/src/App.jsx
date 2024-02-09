95f5773990fccc3d967e04eb087ba3abf866b6b9
Gustavo Pereira <70466896+GAPV-Coder@users.noreply.github.com>
2024-02-09 13:27:54 -0500

    feat: Create navigation bar

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -6,10 +6,12 @@ import SingIn from './pages/SignIn';
 import SingUp from './pages/SignUp';
 import Dashboard from './pages/Dashboard';
 import Projects from './pages/Projects';
+import Header from './components/Header';
 
 const App = () => {
     return (
         <BrowserRouter>
+            <Header />
             <Routes>
                 <Route path="/" element={<Home />} />
                 <Route path="/about" element={<About />} />
