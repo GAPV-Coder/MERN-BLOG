bf703a8010faafafbea4189e96a97bc0e0b1a578
Gustavo Pereira <70466896+GAPV-Coder@users.noreply.github.com>
2024-02-12 18:21:18 -0500

    refactor: Maintain function for user verification

--- server/src/helpers/verifyUser.js
+++ server/src/helpers/verifyUser.js
@@ -1,19 +1,25 @@
 import jwt from 'jsonwebtoken';
 import config from '../config.js';
-import { errorHandler } from './errorHandler.js';
 
 const { jwtSecretKey } = config;
+
+const handlerError = (statusCode, message) => {
+    const error = new Error();
+    error.statusCode = statusCode;
+    error.message = message;
+    return error;
+};
 
 export const verifyToken = (req, res, next) => {
     const token = req.cookies.access_token;
 
     if (!token) {
-        return next(errorHandler(401, 'Unauthorized'));
+        return next(handlerError(401, 'Unauthorized'));
     }
 
     jwt.verify(token, jwtSecretKey, (error, user) => {
         if (error) {
-            return next(errorHandler(401, 'Unauthorized'));
+            return next(handlerError(401, 'Unauthorized'));
         }
 
         req.user = user;
